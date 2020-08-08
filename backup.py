0cc9179d36c6658889e3a18563ab7f52b7a0508c
questras <michu123qwe@gmail.com>
2020-08-08 12:09:52 +0200

    add docstring to functions

--- backup.py
+++ backup.py
@@ -4,6 +4,22 @@ import datetime
 
 
 def create_dirpath_for_old_versions(backup_filepath):
+    """Create directory path for old versions of file specified
+    in backup_filepath.
+    
+    The directory path is the same as backup_filepath, except it is
+    not path to file, but to directory named as follows: 
+    _old_{backup_filepath without extension}
+    the final name is without curly brackets.
+    
+
+    Args:
+        backup_filepath (str): path to file to be backed up.
+
+    Returns:
+        str: path of directory for old versions.
+    """
+    
     backup_filename = os.path.split(backup_filepath)[-1]
     old_version_dirname = '_old_' + backup_filename.split('.')[0]
     old_version_dirpath = os.path.join(
@@ -14,6 +30,19 @@ def create_dirpath_for_old_versions(backup_filepath):
 
 
 def create_filename_for_old_version(backup_filepath):
+    """Create filename for old version of backup_filepath.
+    
+    The old version filename is named as follows:
+    {current_datetime}_{backup_filename}
+    the final name is without curly brackets.
+
+    Args:
+        backup_filepath (str): path to file to be backed up.
+
+    Returns:
+        str: filename of old version of given file.
+    """
+    
     backup_filename = os.path.split(backup_filepath)[-1]
     now = str(datetime.datetime.now())
     
@@ -21,6 +50,17 @@ def create_filename_for_old_version(backup_filepath):
 
 
 def create_directory_for_old_versions(backup_filepath):
+    """Create directory for old versions of file in backup_filepath if
+    it doesn't already exist.
+    
+
+    Args:
+        backup_filepath (str): path to file to be backed up.
+
+    Returns:
+        str: path to created directory for old versions.
+    """
+    
     old_version_dirpath = create_dirpath_for_old_versions(backup_filepath)
     
     if not os.path.isdir(old_version_dirpath):
@@ -30,6 +70,15 @@ def create_directory_for_old_versions(backup_filepath):
     
 
 def create_old_version_of_file(backup_filepath):
+    """Create old version of file in backup_filepath.
+    
+    Copy file in given filepath to directory with old versions
+    and give it proper old version name.
+
+    Args:
+        backup_filepath (str): path to file to be backed up.
+    """
+    
     old_version_dirpath = create_directory_for_old_versions(backup_filepath)
     old_version_filename = create_filename_for_old_version(backup_filepath)
     old_version_filepath = os.path.join(old_version_dirpath, old_version_filename)
@@ -38,6 +87,15 @@ def create_old_version_of_file(backup_filepath):
     
 
 def backup_file(original_filepath, backup_filepath):
+    """Create backup of file in original_filepath and save it
+    to backup_filepath and save older versions of file in
+    backup_filepath if this file has been already backed up.
+
+    Args:
+        original_filepath (str):    path to file to be backed up,
+        backup_filepath (str):      path to file to which the copy 
+                                    will be saved.
+    """
     if not os.path.isfile(original_filepath):
         # Original file doesn't exist.
         return None
@@ -51,6 +109,14 @@ def backup_file(original_filepath, backup_filepath):
 
 
 def backup_directory(original_dirpath, backup_dirpath):
+    """Backup all files in original_dirpath and save them in 
+    backup_dirpath preserving the same file tree. 
+
+    Args:
+        original_dirpath (str):     path to directory to be backed up,
+        backup_dirpath ([type]):    path to directory to which the copies
+                                    will be saved.
+    """
     
     if not os.path.isdir(original_dirpath):
         # Original dir doesn't exist.
